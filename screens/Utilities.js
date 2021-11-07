c01375b5a315fe2ff119110aee25b37dd46dfb13
zajac <michal.chamera@protonmail.com>
2021-11-07 19:27:45 +0100

    added empty screen for utilities.

---
+++ screens/Utilities.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function Login() {
+    return (
+        <View>
+            <Text>Utilities</Text>
+        </View>
+    );
+}
+
