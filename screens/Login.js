8d6c96fee606c06f13e9b7471a7818550bc71a48
zajac <michal.chamera@protonmail.com>
2021-11-07 19:27:06 +0100

    finished login screen by adding socket connection.

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
 
-export default function Login() {
+export default function Login({ navigation }) {
+
+    const [serverIp, setIp] = React.useState('');
+    const [port, setPort] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    async function login() {
+        try {
+            const socket = new WebSocket(`ws://${serverIp}:${port}/${password}`);
+
+            socket.onopen = () => {
+                navigation.navigate('Console', socket);
+            }
+        } catch (error) {
+            alert(error);
+        }
+    }
+
     return (
         <View>
             <Text>rRcon</Text>
-            <Button title='Login' />
+
+            <TextInput
+                placeholder='Ip'
+                value={serverIp}
+                maxLength={15}
+                onChangeText={(val) => setIp(val)}
+                keyboardType='numeric'
+            />
+            <TextInput
+                placeholder='Port'
+                value={port}
+                maxLength={5}
+                onChangeText={(val) => setPort(val)}
+                keyboardType='numeric'
+            />
+            <TextInput
+                placeholder='Password'
+                value={password}
+                maxLength={256}
+                onChangeText={(val) => setPassword(val)}
+                secureTextEntry={true}
+            />
+
+            <Button title='Login' onPress={login} />
         </View>
     );
 }	
