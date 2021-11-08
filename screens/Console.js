74c271d5edb27dda366ed04908e71bf9a4904ac5
zajac <michal.chamera@protonmail.com>
2021-11-08 11:11:45 +0100

    syncing repo with pc so i can continue my work on laptop

--- screens/Console.js
+++ screens/Console.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, FlatList } from 'react-native';
 
-export default function Console() {
+export default function Console({ navigation }) {
+    const socket = navigation.getParam(socket);
+    console.log(socket);
+    const [console, editConsole] = React.useState([
+        {key: '0', time: '11:40' ,message: 'console test'},
+        {key: '1', time: '11:41' ,message: 'krin'},
+        {key: '2', time: '11:59' ,message: 'fgsdfrthrthdf'},
+    ]);
+
+    const openSettings = () => {
+        navigation.navigate('Settings');
+    }
+
+    const openUtilities = () => {
+        navigation.navigate('Utilities');
+    }
+
+    /*const closeSocket = () => {
+        socket.close();
+    }*/
+
+    /*Socket event handling (socket)
+    socket.onmessage = () => {}
+
+    socket.onerror = () => {}
+
+    socket.onclose = (event) => {
+        navigation.navigate('Login');
+        if (event.wasClean) alert('Connection closed.');
+        if (!event.wasClean) alert('Connection closed unexpectedly.');
+    }
+    =====*/
+
     return (
         <View>
             <Text>Console</Text>
+
+            <Button title='Settings' onPress={openSettings} />
+            <Button title='Utilities' onPress={openUtilities} />
+            <Button title='Close connection' onPress={closeSocket} />
+
+            <FlatList
+                data={console}
+                renderItem={({ item }) => (
+                    <Text>{ item.time } --- { item.message }</Text>
+                )}
+            />
         </View>
     );
 } 
